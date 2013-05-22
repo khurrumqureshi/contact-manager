15693480902fe2e2d8f29c42322520aa81707f3d
khurrum qureshi <khurrum.qureshi@goodcoresoft.com>
2013-05-22 18:36:19 +0500

    change url to heroku

--- public/scripts/custom/master.js
+++ public/scripts/custom/master.js
@@ -1,7 +1,7 @@
 
 // global variables used through out the web
 var botsList = {},
-    URL = "http://localhost:3000/"; // ec2 server
+    URL = "http://businesscard-manager.herokuapp.com/"; // ec2 server
 
 function saveContactInfo(event){
     event.preventDefault();
